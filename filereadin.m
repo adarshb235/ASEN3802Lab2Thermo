d0f28c35aee9e3a65dd0500141cc1b74be85a368
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2025-10-02 12:19:40 -0600

    Add files via upload
    updated read file (includes creating data struct and throwing out nans) + driver code

---
+++ filereadin.m
@@ -0,0 +1,39 @@
+function [FinalData] = filereadin() 
+
+    a=dir('*mA');
+
+
+    for i=1:length(a)
+        data = readmatrix(a(i).name);
+        rowsWithNaNs = any(isnan(data), 2);
+        validRows = ~rowsWithNaNs;
+        cleanedData = data(validRows, :);
+        % how to get voltage and amperage from file names?
+        % - options include strsplit, regex, etc.
+        % ultimately, we need to use the format of each file name
+        % 'material'_'volts'V_'amps'mA
+        b = strsplit(a(i).name,'_'); % gives a cell array (b) that is 1x3
+        % {'material','voltsV','ampsmA'} -- now split by 'V' and 'mA'
+        v = strsplit(b{2},'V'); % volts are always in the second portion
+        ampval= strsplit(b{3},'mA'); % amps are always in the third portion
+        volts(i) = str2num(v{1}); % convert string to number (vector)
+        amps(i) = str2num(ampval{1});
+        
+
+        % Store the data from each column into corresponding variables
+        FinalData(i).time = cleanedData(:, 1);
+        FinalData(i).CH1 = cleanedData(:, 2);
+        FinalData(i).CH2 = cleanedData(:, 3);
+        FinalData(i).CH3 = cleanedData(:, 4);
+        FinalData(i).CH4 = cleanedData(:, 5);
+        FinalData(i).CH5 = cleanedData(:, 6);
+        FinalData(i).CH6 = cleanedData(:, 7);
+        FinalData(i).CH7 = cleanedData(:, 8);
+        FinalData(i).CH8 = cleanedData(:, 9);
+      
+
+    end
+
+end
+
+
