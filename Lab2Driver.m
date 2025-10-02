644e09c31936668174611352f9ce92c15a627cb1
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2025-10-02 12:45:36 -0600

    Update Lab2Driver.m
    update to separate lines of data into new cells

--- Lab2Driver.m
+++ Lab2Driver.m
@@ -3,4 +3,16 @@ clear;
 close all;
 
 
-[data] = filereadin();
+[data, filenames] = filereadin();
+
+for i = 1:length(data)
+    time(:, i) = {data(i).time};
+    CH1(:, i) = {data(i).CH1};
+    CH2(:, i) = {data(i).CH2};
+    CH3(:, i) = {data(i).CH3};
+    CH4(:, i) = {data(i).CH4};
+    CH5(:, i) = {data(i).CH5};
+    CH6(:, i) = {data(i).CH6};
+    CH7(:, i) = {data(i).CH7};
+    CH8(:, i) = {data(i).CH8};
+end
