448361312ad49e5da0c8434293ff3f66d1c2b87a
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2025-10-06 23:44:18 -0600

    Update Lab2Driver.m

--- Lab2Driver.m
+++ Lab2Driver.m
@@ -38,8 +38,9 @@ num_cols = ceil(sqrt(num_files));
 num_rows = ceil(num_files / num_cols);
 
 figure(200);
-channelsDistScale = [1.375:0.5:4.875];
-distScale = [0 channelsDistScale 5.875];
+inChannelsDistScale = [1.375:0.5:4.875];
+channelsDistScale = 0.0254 * inChannelsDistScale;
+distScale = 0.0254 * [0 channelsDistScale 5.875];
 sgtitle('Initial Temperature Distribution vs X Location Along L')
 for i = 1:num_files
     fitCoeff(i, :) = polyfit(channelsDistScale, initialTempDist(i, :), 1);
@@ -48,15 +49,20 @@ for i = 1:num_files
     hold on;
     plot(channelsDistScale, initialTempDist(i, :)', 'or');
     plot(distScale, fitDist(i, :), 'b-');
-    plot(distScale, fitDist(i, :) - 2, 'w--');
-    plot(distScale, fitDist(i, :) + 2, 'w--');
+    plot(distScale, fitDist(i, :) - 2, 'k--');
+    plot(distScale, fitDist(i, :) + 2, 'k--');
     givenTitle = append("Material: ", filenames(1, i), ' ', "Voltage: ", filenames(2, i), ' ', "Current: ", filenames(3, i));
     title(givenTitle);
-    legend('Experimental', 'Best Fit', 'Error Bars +/- 2 deg C')
-    xlabel('Position (in)');
+    legend('Experimental', 'Best Fit', 'Error Bars +/- 2 deg C', 'Location', 'best')
+    xlabel('Position (m)');
     ylabel('Temperature (degrees C)');
     hold off;
 end
 M_ext = fitCoeff(:,1);
 hold off;
+
+filename = 'part1task2_initialtempdistfig';
+ax = gca; 
+set(ax, 'LooseInset', get(ax, 'TightInset'));
+print(filename,'-r500','-dpng')
 
